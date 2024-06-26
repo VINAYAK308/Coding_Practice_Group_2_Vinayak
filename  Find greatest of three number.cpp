288e02e9bc4ad38e7c906829961c35d7ec7fc4ab
VINAYAK308 <160124213+VINAYAK308@users.noreply.github.com>
2024-06-26 22:34:35 +0530

    Add files via upload

---
+++  Find greatest of three number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+    cout<<"\n Enter the value of num1: ";
+    cin>>num1;
+    cout<<"\n Enter the value of num2: ";
+    cin>>num2;
+     cout<<"\n Enter the value of num3: ";
+    cin>>num3;
+    if(num1>num2 and num1>num3)
+    {
+        cout<<"\n num1 is greater number";
+    }
+    else if(num2>num3 and num2>num1)
+    {
+        cout<<"\n num2 is greater number";
+    }
+    else
+    {
+        cout<<"\n num3 is greater number";
+    }
+
+
+
+}
